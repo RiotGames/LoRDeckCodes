162617c7d927a1b649724dae1307b204c0740063
jremus <jremus@riotgames.com>
2019-10-15 18:25:46 -0700

    assembly properties.

--- LoRDeckCodes_Tests/Properties/AssemblyInfo.cs
+++ LoRDeckCodes_Tests/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("BaconDeckCodes_Tests")]
+[assembly: AssemblyTitle("LoRDeckCodes_Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaconDeckCodes_Tests")]
+[assembly: AssemblyProduct("LoRDeckCodes_Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
